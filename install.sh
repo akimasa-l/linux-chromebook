a4e6e8b0f579df70a90207143d273f272fbe9748
liumingzheng chromebook <61229liu@seiko.ac.jp>
2021-06-04 14:51:34 +0900

    色々追加するぞ〜〜〜

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
+cd /tmp
 sudo apt update
 sudo apt upgrade -y
-sudo apt install task-japanese fonts-ipafont locales-all
+sudo apt install task-japanese fonts-ipafont locales-all -y
 sudo localectl set-locale LANG=ja_JP.UTF-8
 source /etc/default/locale
-sudo apt install fcitx-mozc
-sudo apt install python3 python3-dev python3-pip
-sudo apt install code-server
+sudo apt install fcitx-mozc -y
+sudo apt install python3 python3-dev python3-pip -y
+sudo apt install code-server -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo bash
+
